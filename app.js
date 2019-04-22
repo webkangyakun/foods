edd935c119b0700c9ec7772d7676faefc5f464f4
kyk <2113563152@qq.com>
2019-04-22 16:48:41 +0800

    111

---
+++ app.js
@@ -0,0 +1,319 @@
+//app_server_00/app.js
+//0:下载mysql express模块
+//1:引入二个模块 mysql express
+const mysql = require("mysql");
+const express = require("express");
+//1.1 引入模块 cors
+const cors = require("cors");
+//
+const bodyParser=require("body-parser");
+const session = require("express-session");
+
+
+//2:创建连接池 很大提高效率方法
+var pool = mysql.createPool({
+  host     : process.env.MYSQL_HOST,
+  port     : process.env.MYSQL_PORT,
+  user     : process.env.ACCESSKEY,
+  password : process.env.SECRETKEY,
+  database : 'app_' + process.env.APPNAME
+});
+
+//创建express对象
+var server=express();
+//配置允许访问列 脚手架8080
+server.use(cors({
+  origin:["http://127.0.0.1:8080",
+  "http://localhost:8080"],
+  credentials:true
+}));
+//配置session
+server.use(session({
+  secret:'128位随机字符串',
+  resave:true,           //每次请求更新
+  saveUninitialized:false ,  //初始化保存数据
+  cookie:{
+    maxAge:1000*60*60  //session对象存活
+  },//不应响session
+}))
+
+//3.2 配置静态资源目录 public
+server.use(express.static("public"));
+//4:为express对象绑定监听端口 3000
+server.listen(5050);
+server.use(bodyParser.urlencoded({
+  extended:false
+}));
+
+//home的f4
+server.get('/hf',(req,res)=>{
+  var sql='select * from food_hf';
+  pool.query(sql,(err,result)=>{
+    if(err) throw err;
+    res.send(result);
+  });
+});
+
+//home的f5
+server.get('/hfive',(req,res)=>{
+  var sql='select * from food_hfive';
+  pool.query(sql,(err,result)=>{
+    if(err) throw err;
+    res.send(result);
+  });
+});
+
+//about   top
+server.get('/abo',(req,res)=>{
+  var sql='select * from food_abone';
+  pool.query(sql,(err,result)=>{
+    if(err) throw err;
+    res.send(result);
+  });
+});
+//about   two
+server.get('/abt',(req,res)=>{
+  var sql='select * from food_abtwo';
+  pool.query(sql,(err,result)=>{
+    if(err) throw err;
+    res.send(result);
+  });
+});
+
+//menu    menum
+server.get('/menum',(req,res)=>{
+  var sql='select * from food_menum';
+  pool.query(sql,(err,result)=>{
+    if(err) throw err;
+    res.send(result);
+  });
+});
+
+//menu   menums
+server.get('/menums',(req,res)=>{
+  var sql='select * from food_menums';
+  pool.query(sql,(err,result)=>{
+    if(err) throw err;
+    res.send(result);
+  });
+});
+
+//menu menuyl
+server.get('/menuyl',(req,res)=>{
+  var sql='select * from food_menuyl';
+  pool.query(sql,(err,result)=>{
+    if(err) throw err;
+    res.send(result);
+  });
+});
+
+//menu menuhb
+server.get('/menuhb',(req,res)=>{
+  var sql='select * from food_menuhb';
+  pool.query(sql,(err,result)=>{
+    if(err) throw err;
+    res.send(result);
+  });
+});
+
+//detail
+server.get('/detail',(req,res)=>{
+  var sql='select * from food_detail';
+  pool.query(sql,(err,result)=>{
+    if(err) throw err;
+    res.send(result);
+  });
+});
+
+//cart
+server.get('/cart',(req,res)=>{
+  var sql='select * from food_cart';
+  pool.query(sql,(err,result)=>{
+    if(err) throw err;
+    res.send(result);
+  });
+});
+
+//用户点击添加购物车
+server.get("/addCart",(req,res)=>{
+  //alter table xz_cart add count INT;
+  //update xz_cart set count=1;
+  //var uid=1;
+   var id=req.query.id;
+   var sql='select img,title from food_menums where id=? ';
+   pool.query(sql,[id],(err,result)=>{
+    if(err)throw err;
+  console.log(result);
+       console.log(result.length)
+         if(result.length-1==0){
+           var sql=`insert into food_cart values(null,'${result[0].img}','${result[0].title}',1)`;
+          //  console.log(sql)
+         }else{
+           var sql=`update food_cart set count=count+1 where id =${id}`;  
+         }
+         console.log(sql);
+         pool.query(sql,(err,result)=>{
+          if(err) throw err;
+          res.send({code:1,msg:'OK'});
+        })      
+ });
+  // var sql='insert into xz_cart(id,uid,pid,pname,price,count) values(null,?)';
+  // var sql ='update xz_cart set count=count+1 where uid = ? and pid=?';
+});
+
+//删除购物车中某个商品
+server.post('/removeItem',(req,res)=>{
+  var id=req.body.id;
+  var sql="delete from food_cart where id=?";
+  id=parseInt(id);
+  pool.query(sql,[id],(err,result)=>{
+    if(err)throw err;
+    //执行SQL语句影响行数
+    if(result.affectedRows>0){
+      res.send({code:1,msg:"删除成功"});
+    }else{
+      res.send({code:-1,msg:'删除失败'});
+    }
+  })
+})
+
+
+
+
+
+//注册
+server.get("/register",(req,res)=>{
+  var zphone=req.query.zphone;
+  var zpwd=req.query.zpwd;
+  var email=req.query.email;
+  var sql="insert into food_register values(null,?,?,?)"
+  pool.query(sql,[zphone,zpwd,email],(err,result)=>{
+    if(err) throw err;
+  })
+});
+
+
+//用户登录
+//1:用户get 请求路径/login
+server.get("/login",(req,res)=>{
+  //2:获取二个参数 uname upwd
+  var zphone = req.query.lphone;
+  var zpwd = req.query.lpwd;
+  //3:创建sql
+  var sql = "SELECT * FROM food_register where zphone=? and zpwd=?";
+  console.log(sql)
+  //4:执行sql
+  pool.query(sql,[zphone,zpwd],(err,result)=>{
+      if(err)throw err;
+      console.log(result)
+      //5:获取数据库返回结果
+      //6:返回客户数据
+      if(result.length==0){
+        res.send({code:-1,msg:"用户名或密码有误"});
+      }else{
+        res.send({code:1,msg:"登录成功"});
+      }
+  })
+});
+
+//添加评论
+server.post("/addcomment",(req,res)=>{
+  var title=req.body.comment;
+  console.log(title);
+  var sql=`insert into food_comment values (null,'http://127.0.0.1:3000/img/food-comment/uid1.jpg',?)`;
+  pool.query(sql,[title],(err,result)=>{
+       if(err)throw err;
+       res.send({code:1,msg:'添加成功'})
+  })
+})
+
+//加载评论
+server.get('/getcomment',(req,res)=>{
+  var pno=req.query.pno;
+  var pageSize
+  if(!pno){
+    pno=1;
+  }
+  if(!pageSize){
+    pageSize=3;
+  }
+  var sql=`select * from food_comment LIMIT ?,? `;
+  var offset=(pno-1)*pageSize;
+  pageSize=parseInt(pageSize);
+  console.log(pageSize);
+  pool.query(sql,[offset,pageSize],(err,result)=>{
+    if(err)throw err;
+    res.send({code:1,data:result});
+  })
+})
+
+
+
+//功能二:商品分页显示
+//用户get 请求路径 /getProducts
+server.get("/getProducts",(req,res)=>{
+ //1:获取用户参数 pno pageSize
+ var pno = req.query.pno;
+ var pageSize = req.query.pageSize;
+ //2:为参数设置默认值  pno:1 pageSize:4
+ if(!pno){pno=1}
+ if(!pageSize){pageSize=4}
+ //3:创建sql语句
+ var sql = "";
+ sql+=" SELECT l.lid,l.lname,l.price,p.md";
+ sql+=" from xz_laptop l,xz_laptop_pic p";
+ sql+=" WHERE l.lid = p.laptop_id";
+ sql+=" GROUP BY l.lid";
+ sql+=" LIMIT ?,?";
+ //3.1:计算开始记录数,几条记录  
+ var offset = (pno-1)*pageSize;
+ pageSize = parseInt(pageSize);
+ //4:执行sql语句
+ pool.query(sql,[offset,pageSize],(err,result)=>{
+   if(err)throw err;
+   res.send({code:1,data:result});
+ //5:获取数据库返回结果并且发送脚手架
+ })
+});
+//功能三:首页轮播图
+server.get("/imglist",(req,res)=>{
+  var rows = [
+    {id:1,img_url:"http://127.0.0.1:3000/img/index/banner1.jpg"},
+    {id:2,img_url:"http://127.0.0.1:3000/img/index/banner2.jpg"},
+    {id:3,img_url:"http://127.0.0.1:3000/img/index/banner3.jpg"},
+  ];
+  res.send({code:1,data:rows});
+});
+//功能四:首页九宫格
+server.get("/grid",(req,res)=>{
+  var rows = [
+    {id:1,title:"饮料饮品",img_url:"http://127.0.0.1:3000/img/grid/menu1.png"},
+    {id:2,title:"汉堡快餐",img_url:"http://127.0.0.1:3000/img/grid/menu2.png"},
+    {id:3,title:"新鲜水果",img_url:"http://127.0.0.1:3000/img/grid/menu3.png"},
+    {id:4,title:"糖果点心",img_url:"http://127.0.0.1:3000/img/grid/menu4.png"},
+    {id:5,title:"中式美食",img_url:"http://127.0.0.1:3000/img/grid/menu5.png"},
+    {id:6,title:"调味食品",img_url:"http://127.0.0.1:3000/img/grid/menu6.png"},            
+  ];
+  res.send(rows);
+});
+//功能五:新闻分页显示
+server.get("/news",(req,res)=>{
+  //*参数    pno 页码 pageSize 页大小
+  var pno = req.query.pno;
+  var pagSize = req.query.pageSize;
+  //*默认值  1   7
+  if(!pno){pno=1}
+  if(!pageSize){pageSize=7}
+  var sql = " SELECT id,title,ctime,";
+      sql +=" point,img_url";
+      sql +=" FROM xz_news";
+      sql +=" LIMIT ?,?";
+  var offset = (pno-1)*pageSize;
+  pageSize = parseInt(pageSize);
+  pool.query(sql,[offset,pageSize],(err,result)=>{
+       if(err)throw err;
+       res.send({code:1,data:result});
+  });    
+  //*json   {code:1,data:[]}
+});
+
---
+++ db.sql
@@ -0,0 +1,180 @@
+
+#创建home的f4表
+CREATE TABLE food_hf(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  img_url varchar(255),
+  title varchar(255)
+);
+INSERT INTO food_hf VALUES(null,'img/homef4/hf1.jpg','以简单为美味，以自然为美味,美食简约哲学的极致表现');
+INSERT INTO food_hf VALUES(null,'img/homef4/hf2.jpg','不仅仅是简单的味觉感受，更是一种精神享受');
+INSERT INTO food_hf VALUES(null,'img/homef4/hf3.jpg','品味更多美食，享受更多健康');
+select * from  food_hf;
+
+#home的F5
+CREATE TABLE food_hfive(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  logo varchar(255),
+  title varchar(255),
+  img_url varchar(255)
+);
+INSERT INTO food_hfive VALUES(null,'img/homef5/xxj.png','竹雨凇风琴韵，茶点梧月书声','img/homef5/xxj.gif');
+INSERT INTO food_hfive VALUES(null,'img/homef5/ycyt.png','茶香型麻辣烫汤底及绿茶植物油的概念','img/homef5/ycyt.gif');
+INSERT INTO food_hfive VALUES(null,'img/homef5/ttxs.png','岁月如歌，湘味永恒
+舌尖上的湘与乡愁','img/homef5/ttxs.gif');
+select * from food_hfive;
+
+
+
+#创建about的表
+CREATE TABLE food_abone(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  img_url varchar(255),
+  title varchar(255)
+);
+INSERT INTO food_abone VALUES(null,'img/about/daily-1.jpg','“Daily” is a place you can visit on a daily basis in order to drink coffee, to have a chat, to work or just to spend some good time. This coffee place has an arthouse movie atmosphere. From every perspective, you have a sense of a movie episode where people casually interact with each other.');
+INSERT INTO food_abone VALUES(null,'img/about/daily-2.jpg','«Daily» is not just a coffee shop, but also kind of urban space, offering some social events like ‘’Breakfast with an interesting person”, happening once in two weeks, hosting different famous people sharing some inspiring speeches with guests.');
+select * from  food_abone;
+
+#创建about的第二个表
+CREATE TABLE food_abtwo(
+   id INT PRIMARY KEY AUTO_INCREMENT,
+   img_url varchar(255)
+);
+INSERT INTO food_abtwo VALUES(null,'img/about/daily-3.jpg');
+INSERT INTO food_abtwo VALUES(null,'img/about/daily-4.jpg');
+INSERT INTO food_abtwo VALUES(null,'img/about/daily-5.jpg');
+INSERT INTO food_abtwo VALUES(null,'img/about/daily-6.jpg');
+INSERT INTO food_abtwo VALUES(null,'img/about/daily-7.jpg');
+INSERT INTO food_abtwo VALUES(null,'img/about/daily-8.jpg');
+INSERT INTO food_abtwo VALUES(null,'img/about/daily-9.jpg');
+INSERT INTO food_abtwo VALUES(null,'img/about/daily-10.jpg');
+INSERT INTO food_abtwo VALUES(null,'img/about/daily-11.jpg');
+select * from  food_abtwo;
+
+#创建menu-m
+CREATE TABLE food_menum(
+   id INT PRIMARY KEY AUTO_INCREMENT,
+   img varchar(255)
+);
+INSERT INTO food_menum VALUES(null,'img/menu-m/featured-1-mobile.jpg');
+INSERT INTO food_menum VALUES(null,'img/menu-m/cake2.gif');
+INSERT INTO food_menum VALUES(null,'img/menu-m/cafe.gif');
+INSERT INTO food_menum VALUES(null,'img/menu-m/cake.gif');
+INSERT INTO food_menum VALUES(null,'img/menu-m/hanbao.gif');
+select * from  food_menum;
+
+#创建menu-ms
+CREATE TABLE food_menums(
+     id INT PRIMARY KEY AUTO_INCREMENT,
+     img varchar(255),
+     title varchar(255)
+);
+INSERT INTO food_menums VALUES(null,'img/menu-ms/ms1.jpg','美式松饼');
+INSERT INTO food_menums VALUES(null,'img/menu-ms/ms2.jpg','蓝莓麦芬');
+INSERT INTO food_menums VALUES(null,'img/menu-ms/ms3.jpg','巧克力麦芬');
+INSERT INTO food_menums VALUES(null,'img/menu-ms/ms4.jpg','旋风玉桂酥');
+INSERT INTO food_menums VALUES(null,'img/menu-ms/ms5.jpg','法式香酥可颂');
+INSERT INTO food_menums VALUES(null,'img/menu-ms/ms6.jpg','法式焦糖酥');
+INSERT INTO food_menums VALUES(null,'img/menu-ms/ms7.jpg','榛子果仁酥');
+INSERT INTO food_menums VALUES(null,'img/menu-ms/ms8.jpg','蜂蜜提子司康');
+INSERT INTO food_menums VALUES(null,'img/menu-ms/ms9.jpg','燕麦焦糖布丁');
+INSERT INTO food_menums VALUES(null,'img/menu-ms/ms10.jpg','提子干松饼');
+select * from food_menums;
+
+#创建menu-yl
+CREATE TABLE food_menuyl(
+     id INT PRIMARY KEY AUTO_INCREMENT,
+     img varchar(255),
+     title varchar(255)
+);
+INSERT INTO food_menuyl VALUES(null,'img/menu-yl/yl1.jpg','阿馥奇朵');
+INSERT INTO food_menuyl VALUES(null,'img/menu-yl/yl2.jpg','红茶拿铁');
+INSERT INTO food_menuyl VALUES(null,'img/menu-yl/yl3.jpg','美式咖啡');
+INSERT INTO food_menuyl VALUES(null,'img/menu-yl/yl4.jpg','摩卡');
+INSERT INTO food_menuyl VALUES(null,'img/menu-yl/yl5.jpg','拿铁');
+INSERT INTO food_menuyl VALUES(null,'img/menu-yl/yl6.jpg','卡布奇诺');
+INSERT INTO food_menuyl VALUES(null,'img/menu-yl/yl7.jpg','冰摇柚柚蜂蜜红茶');
+INSERT INTO food_menuyl VALUES(null,'img/menu-yl/yl8.jpg','冰摇红梅黑加仑');
+INSERT INTO food_menuyl VALUES(null,'img/menu-yl/yl9.jpg','冰摇芒果花草');
+INSERT INTO food_menuyl VALUES(null,'img/menu-yl/yl10.jpg','冰摇桃桃乌龙');
+select * from food_menuyl;
+
+#创建menu-hb
+CREATE TABLE food_menuhb(
+     id INT PRIMARY KEY AUTO_INCREMENT,
+     img varchar(255),
+     title varchar(255)
+);
+INSERT INTO food_menuhb VALUES(null,'img/menu-hb/hb1.jpg','牛油果鸡肉培果');
+INSERT INTO food_menuhb VALUES(null,'img/menu-hb/hb2.jpg','培根蛋可颂堡');
+INSERT INTO food_menuhb VALUES(null,'img/menu-hb/hb3.jpg','蜜汁培根蛋卷');
+INSERT INTO food_menuhb VALUES(null,'img/menu-hb/hb4.jpg','层层牛肉法棍');
+INSERT INTO food_menuhb VALUES(null,'img/menu-hb/hb5.jpg','牛肉芝士可颂');
+INSERT INTO food_menuhb VALUES(null,'img/menu-hb/hb6.jpg','凯撒鸡肉卷');
+INSERT INTO food_menuhb VALUES(null,'img/menu-hb/hb7.jpg','鸡肉芝香帕尼尼');
+INSERT INTO food_menuhb VALUES(null,'img/menu-hb/hb8.jpg','炒蛋菌菇虾仁卷');
+INSERT INTO food_menuhb VALUES(null,'img/menu-hb/hb9.jpg','火腿芝士可颂');
+INSERT INTO food_menuhb VALUES(null,'img/menu-hb/hb10.jpg','帕斯雀牛肉三明治');
+select * from food_menuhb;
+
+#创建detail
+CREATE TABLE food_detail(
+     id INT PRIMARY KEY AUTO_INCREMENT,
+     imga varchar(255),
+     titlea varchar(255),
+
+     imgb varchar(255),
+     titleb varchar(255),
+     imgb_text varchar(255),
+     titleb2 varchar(255),
+
+     imgc varchar(255),
+     titlec varchar(255),
+     imgc_text varchar(255),
+     titlec2 varchar(255),
+
+     imgd varchar(255),
+     titled varchar(255),
+     imgd_text varchar(255),
+     titled2 varchar(255),
+
+     imge varchar(255),
+     titlee varchar(255),
+     imge_text varchar(255),
+     titlee2 varchar(255)
+);
+INSERT INTO food_detail VALUES(null,'img/detail/d1.jpg','灵感源自臻选®烘焙工坊，咖啡大师的巧思探索，创意突破咖啡常态，在与冰淇淋的巧妙搭配中，创造专属味蕾体验——为我们钟爱的咖啡，精心调制出彰显其个性风味的特调配方，品尝那一刻，瞬间沉醉在咖啡与冰淇淋的奇妙融合中。','img/detail/d2.jpg','阿馥奇朵™','img/detail/affogato.png','双份精萃浓缩咖啡，新西兰进口香草冰淇淋','img/detail/d3.jpg','麦芽雪冷萃™','img/detail/coldbrewfloat.png','冷萃咖啡,巧克力味苦味酒，麦芽成分','img/detail/d4.jpg','冷萃浮乐朵™','img/detail/coldbrewmalt.png','冷萃咖啡，新西兰进口香草冰淇淋','img/detail/d5.jpg','气致™冷萃浮乐朵™','img/detail/nitorcoldbrewfloat.png','气致™冷萃咖啡，新西兰进口香草冰淇淋');
+select * from food_detail;
+
+#创建购物车表
+CREATE TABLE food_cart(
+     id INT PRIMARY KEY AUTO_INCREMENT,  
+     img varchar(255),
+     title varchar(255),
+     count int
+);
+
+#注册表
+CREATE TABLE food_register(
+     id INT PRIMARY KEY AUTO_INCREMENT,  
+     zphone  varchar(255),
+     zpwd varchar(225),
+     email varchar(225)
+);
+
+#评论表
+CREATE TABLE food_comment(
+     id INT PRIMARY KEY AUTO_INCREMENT,  
+     img   varchar(255),
+     title  varchar(255)
+);
+INSERT INTO  food_comment VALUES(null,"img/food-comment/uid1.jpg","味道真棒唉111");
+INSERT INTO  food_comment VALUES(null,"img/food-comment/uid1.jpg","味道真棒唉222"); 
+INSERT INTO  food_comment VALUES(null,"img/food-comment/uid1.jpg","味道真棒唉333"); 
+INSERT INTO  food_comment VALUES(null,"img/food-comment/uid2.jpg","味道真棒唉444"); 
+INSERT INTO  food_comment VALUES(null,"img/food-comment/uid2.jpg","味道真棒唉555"); 
+INSERT INTO  food_comment VALUES(null,"img/food-comment/uid2.jpg","味道真棒唉666"); 
+INSERT INTO  food_comment VALUES(null,"img/food-comment/uid3.jpg","味道真棒唉777"); 
+INSERT INTO  food_comment VALUES(null,"img/food-comment/uid3.jpg","味道真棒唉888"); 
+INSERT INTO  food_comment VALUES(null,"img/food-comment/uid3.jpg","味道真棒唉999"); 
+select * from food_comment;
